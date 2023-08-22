1c999d9357c08d8b73ef648ad9092b7e4f06a0fe
Santiago Caritat <6317@holbertonstudents.com>
2023-08-22 07:10:44 -0700

    task 1

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -14,6 +14,6 @@ if __name__ == "__main__":
     with open('{}.csv'.format(id), 'w') as file_csv:
         for id_csv in response_todos:
             if id_csv['userId'] == id:
-                file_csv.write('"{}", "{}", "{}", "{}"\n'.format(
+                file_csv.write('"{}","{}","{}","{}"\n'.format(
                     id, response_users['username'],
                     id_csv['completed'], id_csv['title']))
