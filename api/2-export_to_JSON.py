c77d11f87b20b0f9fbed99070522361e7112e4e4
Santiago Caritat <6317@holbertonstudents.com>
2023-08-22 07:05:10 -0700

    task 2

---
+++ api/2-export_to_JSON.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""task 0"""
+
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    users = "https://jsonplaceholder.typicode.com/users/{}"
+    todos = "https://jsonplaceholder.typicode.com/todos/?userId={}"
+
+    id = int(sys.argv[1])
+    response_users = requests.get(users.format(id)).json()
+    response_todos = requests.get(todos.format(id)).json()
+    with open('{}.json'.format(id), 'w') as file_csv:
+        _list = []
+        for id_csv in response_todos:
+            if id_csv['userId'] == id:
+                _list.append(
+                    {"task": id_csv['title'],
+                     "completed": id_csv['completed'],
+                     "username": response_users['username']})
+    json.dump({'{}'.format(id): _list}, file_csv)
